1ebebad9f538c9505395dc092ce2e307f1a4038f
mehrnazsadrol <84254284+mehrnazsadrol@users.noreply.github.com>
2023-08-03 11:32:30 -0700

    finalized

--- project_two.py
+++ project_two.py
@@ -4,7 +4,8 @@ from sklearn.ensemble import GradientBoostingRegressor
 from sklearn.model_selection import train_test_split 
 from scipy.stats import pearsonr, spearmanr
 import matplotlib.pyplot as plt
-from statsmodels.tsa.seasonal import seasonal_decompose
+from scipy import stats
+
 
 df = pd.read_csv("Failed_Bank_Dataset_2.csv")
 
@@ -64,37 +65,69 @@ print(f"Spearman correlation between city and closing date is : {spearman_cor:.4
 # pearson correlation between city and clsoing date is : -0.6638
 # Spearman correlation between city and closing date is : -0.7848
 # we can see there is no close correlation between the city and closing date 
-
-
-#since I can't find a correlation between the columns. I will try to do time series analysis
+#
+#
+#
+# I want to see if there is a seasonality of closing dates
 
 data_frame = df.copy()
 data_frame['Closing_Date'] = pd.to_datetime(data_frame['Closing_Date'])
-data_frame.sort_values(by='Closing_Date',inplace=True)
-
-# plt.figure(figsize=(12,6))
-# plt.plot(data_frame['Closing_Date'],data_frame['Total_Deposits'],label='Total Deposits')
-# plt.plot(data_frame['Closing_Date'],data_frame['Total_Assets'],label='Total Assets')
-# plt.xlabel('Closing date')
-# plt.ylabel('Amount')
-# plt.title('Total deposits and total assets over time')
-# plt.legend()
-# plt.show()
-
-#refer to  PATH = ("figure_one.png") for the plot. 
-
-#again nothing usefull was achieved from this analysis. 
-# I want to see if there is a seasonality of closing dates
-
 data_frame['Month'] = data_frame['Closing_Date'].dt.month
-print (data_frame['Month'].head())
 monthly_closure = data_frame['Month'].value_counts().sort_index()
-print (monthly_closure)
+all_months = pd.Series(range(1, 13))
 plt.figure(figsize=(12,6))
-plt.plot(kind='bar',color='blue')
+monthly_closure.plot(kind='bar',color='blue')
 plt.xlabel('Month')
 plt.ylabel('Number of Closures')
 plt.title('Number of Bank Closures in Each Month')
-plt.xticks(range(1, 13), ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'])
+plt.xticks(range(0, 12), ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'])
 plt.grid(axis='y')
+plt.show()
+
+#refer to  PATH = ("figure_2.png") for the plot. 
+#
+#
+#
+# I want to see if the distribution of states and the closing dates
+
+unique_states = data_frame['State'].unique()
+states_to_num = {state: i for i,state in enumerate(unique_states)}
+data_frame['State'] = data_frame['State'].map(states_to_num)
+
+states = data_frame['State'].value_counts().sort_index()
+all_states= pd.Series(range(1, 45))
+plt.figure(figsize=(12,6))
+states.plot(kind='bar',color='blue')
+plt.xlabel('Month')
+plt.ylabel('Number of Closures')
+plt.title('Number of Bank Closures in Each State')
+plt.xticks(range(0, 44), ['HI','IL','NH','OH','AR','FL','TX','AZ','MI','CT','GA','TN','LA','CA','WI', 'PA', 'NJ', 'NY' ,'UT' ,'MO' ,'MN' ,'NV' ,'KS', 'WV', 'WA', 'MD' ,'OR' ,'NE', 'CO' ,'NC', 'ID', 'WY' ,'SD' ,'OK' ,'AL' ,'IA' ,'IN' ,'KY' ,'VA' ,'NM', 'SC' ,'MA', 'PR' ,'MS'])
+plt.grid(axis='y')
+plt.show()
+
+#refer to  PATH = ("figure_3.png") for the plot. 
+#
+#
+#
+# I want to see if there are outliers in total deposits and assets
+
+z_score_assets = stats.zscore(data_frame['Total_Assets'])
+z_score_deposits = stats.zscore(data_frame['Total_Deposits'])
+threshold = 3
+outliers_deposits = data_frame[abs(z_score_deposits) > threshold]
+outliers_assets = data_frame[abs(z_score_assets) > threshold]
+print("---------------------")
+print("Outliers in Total_Deposits:",outliers_deposits[['Bank_Name','City',"Total_Deposits"]])
+print("\nOutliers in Total_Assets:",outliers_assets[['Bank_Name','City',"Total_Assets"]])
+
+# Three outlier banks:
+# 1 - Signature Bank
+# 2 - Silicon Valley Bank 
+# 3 - Washington Mutual Bank (Including its subsidiary Washington Mutual Bank FSB)
+# this also is obvious from the plot
+plt.figure(figsize=(8, 6))
+plt.scatter(data_frame['Total_Deposits'], data_frame['Total_Assets'])
+plt.xlabel('Total_Deposits')
+plt.ylabel('Total_Assets')
+plt.title('Scatter Plot of Total_Deposits vs. Total_Assets')
 plt.show()
