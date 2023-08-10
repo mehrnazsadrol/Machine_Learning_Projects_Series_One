ca331224e32944929db188e659026b0b49b0eb7d
mehrnazsadrol <84254284+mehrnazsadrol@users.noreply.github.com>
2023-08-09 18:46:55 -0700

    analyzed color

--- project_four.py
+++ project_four.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import matplotlib.pyplot as plt 
 from PIL import Image
+import cv2
+import os
 
 path = './game-of-deep-learning-ship-datasets/train/images/'
 df = pd.read_csv('./game-of-deep-learning-ship-datasets/train/train.csv')
@@ -17,31 +19,31 @@ test_df = pd.read_csv('./game-of-deep-learning-ship-datasets/test_ApKoW4T.csv')
 test_df['path'] = path + test_df['image']
 
 
-# plt.figure(figsize = (15,12))
-# for idx,image_path in enumerate(df['path']):
-#     if idx==24:
-#         break
-#     plt.subplot(4,8,idx+1)
-#     img = Image.open(image_path)
-#     img = img.resize((224,224))
-#     plt.imshow(img)
-#     plt.axis('off')
-#     plt.title(idx)
-# plt.tight_layout()
-# plt.show()
+plt.figure(figsize = (15,12))
+for idx,image_path in enumerate(df['path']):
+    if idx==24:
+        break
+    plt.subplot(4,8,idx+1)
+    img = Image.open(image_path)
+    img = img.resize((224,224))
+    plt.imshow(img)
+    plt.axis('off')
+    plt.title(idx)
+plt.tight_layout()
+plt.show()
 
-# plt.figure(figsize = (15,12))
-# for idx,image_path in enumerate(test_df['path']):
-#     if idx==24:
-#         break
-#     plt.subplot(4,8,idx+1)
-#     img = Image.open(image_path)
-#     img = img.resize((224,224))
-#     plt.imshow(img)
-#     plt.axis('off')
-#     plt.title(idx)
-# plt.tight_layout()
-# plt.show()
+plt.figure(figsize = (15,12))
+for idx,image_path in enumerate(test_df['path']):
+    if idx==24:
+        break
+    plt.subplot(4,8,idx+1)
+    img = Image.open(image_path)
+    img = img.resize((224,224))
+    plt.imshow(img)
+    plt.axis('off')
+    plt.title(idx)
+plt.tight_layout()
+plt.show()
 
 counts = df['classes'].value_counts()
 print (counts)
@@ -50,3 +52,31 @@ name = counts.index
 plt.show()
 
 
+widths, heights = [], []
+for i,path in enumerate(df["path"]):
+    width, height = Image.open(path).size
+    widths.append(width)
+    heights.append(height)
+    
+df["width"] = widths
+df["height"] = heights
+
+print (df['width'].describe())
+print (df['height'].describe())
+
+
+r,g,b = [],[],[]
+
+for i,row in df.iterrows(): 
+    my_image = cv2.imread(row['path'])
+    rgb = my_image.mean(axis=(0,1))
+    r.append(rgb[2])
+    g.append(rgb[1])
+    b.append(rgb[0])
+
+df['red'] = r
+df['green'] = g
+df['blue'] = b
+
+
+print (df[['red','green','blue','image']].head())
